1a9c2a2bc1d7fab67d0a1a2ba441dffdda556a7e
airKing05 <anil050797@gmail.com>
2022-08-13 00:29:41 +0530

    playboard ready

--- ludo-multiplayer/src/PlayBoard.js
+++ ludo-multiplayer/src/PlayBoard.js
@@ -6,11 +6,26 @@ import TrackVertical from './components/TrackVertical'
 
 export default function PlayBoard() {
   return (
-    <div className='container' style={{maxWidth: '1500px'}}>
+    <div className='' style={{maxWidth: '1500px'}}>
+      <div className='row d-flex justify-content-center' >
+        <PawnsHome />
+        <TrackVertical />
+        <PawnsHome />
+      </div>
+      <div className='row d-flex justify-content-center' >
+        <TrackHorizontal />
+        <PawnsPort />
+        <TrackHorizontal />
+      </div>
+      <div className='row d-flex justify-content-center' >
+        <PawnsHome />
+        <TrackVertical />
+        <PawnsHome />
+      </div>
       {/* <TrackHorizontal/> */}
       {/* <PawnsHome/> */}
       {/* <TrackVertical/> */}
-      <PawnsPort/>
+      {/* <PawnsPort/> */}
     </div>
   )
 }
