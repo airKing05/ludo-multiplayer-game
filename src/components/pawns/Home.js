f1f4e89bbda371e909921c5713fa7c4f30301246
akshat <akshat@doutbnut.com>
2022-08-16 11:54:44 +0530

    .

--- src/components/pawns/Home.js
+++ src/components/pawns/Home.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { move } from '../../helper/movements';
+import { useRef } from 'react';
 
 export default function Home() {
+  const pawnElement=  useRef();
+  let x = -1;
+  let y = -6;
+  const gridWidth = 50;
+  const gridHeight = 50;
+  const movePawn = () => {
+    let [_x, _y] = move(x, y);
+    x += _x;
+    y += _y;
+    const translateX = _x * gridWidth * (-1 * _x);
+    const translateY = _y * gridHeight * (-1 * _y);
+    console.log(_x, _y, x, y, translateX, translateY);
+
+    pawnElement.current.style.transform += `translate(${translateX}px, ${translateY}px)` 
+
+  }
+
+
   return (
-      <div style={{position: 'relative', width: '150px', height: '150px', padding: '0px'}}>
-          <div className='triangle-down' style={{position: 'absolute', top:'0px'}}></div>
-          <div className='triangle-up' style={{position: 'absolute', bottom:'0px'}}></div>
-          <div className='triangle-right'></div>
-          <div className='triangle-left' style={{position: 'absolute', right:'0px', top: '0px'}}></div>      
-    </div>
+      <Fragment>
+          <div style={{position: 'relative', width: '150px', height: '150px', padding: '0px'}}>
+            <div className='triangle-down' style={{position: 'absolute', top:'0px'}}>
+                <div style={{width:'50px', height: '50px', background:'black', position:'absolute', zIndex:100, left: '-75px', top:'275px'}} onClick={movePawn} ref={pawnElement}></div>
+            </div>
+
+            <div className='triangle-up' style={{position: 'absolute', bottom:'0px'}}>
+              {/* <div style={{width:'50px', height: '100px', background:'black', position:'absolute', zIndex:100}}></div> */}
+            </div>
+            <div className='triangle-right'></div>
+            <div className='triangle-left' style={{position: 'absolute', right:'0px', top: '0px'}}></div>      
+          </div>
+      </Fragment>
+      
   )
 }
--- src/helper/movements.js
+++ src/helper/movements.js
@@ -1,7 +1,70 @@
-export function getMovement(currRow, currCol, pawn) {
-    // if (currRow == 1 && ) {
-    //     return ;
-    // }
+export function move(x, y, pawn) {
+
+    // check for home entry points
+    if (x === 0 && y === 7 ) {
+        return [1, 0];
+    }
+
+    if (x === 0 && y === -7 ) {
+        return [-1, 0];
+    }
+
+    if (x === 7 && y === 0 ) {
+        return [0, -1];
+    }
+
+    if (x === -7 && y === 0 ) {
+        return [0, 1];
+    }
+
+
+    // check for diagonal movement points
+    if (x=== -1 && y === -2) {
+        return [-1, 1];
+    }
+
+    if (x=== -2 && y === 1) {
+        return [1, 1];
+    }
+
+    if (x=== 1 && y === 2) {
+        return [1, -1];
+    }
+
+    if (x=== 2 && y === -1) {
+        return [-1, -1];
+    }
+
+
+    // check for corners rotation
+    if ((x === -7 && y === -1) || (x === -1 && y === -7)) {
+        return [0, 1];
+    }
+
+    if ((x === -7 && y === 1) || (x === -1 && y === 7)) {
+        return [1, 0];
+    }
+
+    if ((x === 1 && y === 7) || (x === 7 && y === 1)) {
+        return [0, -1];
+    }
+
+    if ((x === 7 && y === -1) || (x === 1 && y === -7)) {
+        return [1, 0];
+    }
+
+
+    // normal movement
+    if (x === -1) {
+        return [0, 1];
+    } else if (x === -1) {
+        return [0, -1];
+    } else if (y === 1) {
+        return [1, 0];
+    } else if (y === -1) {
+        return [-1, 0];
+    }
+    return [0, 0];
 }
 
 
